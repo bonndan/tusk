7ab732d2b6c24e047bbbaed76bc9f5e37c7eeded
Daniel Pozzi <bonndan76@googlemail.com>
2016-12-06 21:30:32 +0100

    altered configuration (work in progress)

--- src/Tusk/Configuration.php
+++ src/Tusk/Configuration.php
@@ -9,18 +9,53 @@ namespace Tusk;
  */
 class Configuration
 {
-    public $packageBaseDir;
-    public $namespaceBaseDir;
+    /**
+     * source directory
+     * @var string
+     */
+    public $source;
+    
+    /**
+     * target directory
+     * @var string
+     */
+    public $target;
+    
+    /**
+     * @var string[] source dir => target namespace
+     */
+    public $namespaces = [];
+    
+    /**
+     * source dir => target dir under resources
+     * @var string[]
+     */
+    public $resources = [];
+    
+    /**
+     * source dir => target dir under project
+     * @var string[]
+     */
+    public $other = [];
     
     public function isConfigured() : bool
     {
-        return $this->packageBaseDir && $this->namespaceBaseDir;
+        return $this->source && $this->target;
     }
 
+    /**
+     * Factory method, accepts an assoc array with keys matching this fields.
+     * 
+     * @param array $options
+     * @return \Tusk\Configuration
+     */
     public static function create(array $options) : Configuration
     {
         $config = new Configuration();
         foreach ($options as $key => $value) {
+            if (!property_exists(Configuration::class, $key))
+                throw new \InvalidArgumentException("Unknown configuration property $key");
+            
             $config->$key = $value;
         }
         return $config;
