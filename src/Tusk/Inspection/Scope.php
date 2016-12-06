c3532aae5f92a96d90e3e8ccff341a3fd557833c
Daniel Pozzi <bonndan76@googlemail.com>
2016-12-06 21:29:56 +0100

    scope improvements

--- src/Tusk/Inspection/Scope.php
+++ src/Tusk/Inspection/Scope.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tusk\Inspection;
 
 /**
@@ -9,24 +10,25 @@ namespace Tusk\Inspection;
  */
 class Scope
 {
+
     const SCOPE = 'scope';
-    
+
     /**
      * @var \PhpParser\Node|\PhpParser\Node\FunctionLike
      */
     private $root;
-    
+
     /**
      * scope variables
      * @var \PhpParser\Node[]
      */
     private $vars = [];
-    
+
     public function __construct(\PhpParser\Node $scopeRoot = null)
     {
         $this->root = $scopeRoot;
     }
-    
+
     /**
      * Add a variable to the scope (node name is taken as index).
      * 
@@ -40,7 +42,7 @@ class Scope
             return;
         $this->vars[$name] = $node;
     }
-    
+
     /**
      * Registers the scope to the node attribute "scope".
      * 
@@ -60,53 +62,64 @@ class Scope
      * @param Node|string $node
      * @return bool
      */
-    public function hasVar($node) : bool
+    public function hasVar($node): bool
     {
         if (is_string($node)) {
             return array_key_exists($node, $this->vars);
         }
-        
+
         $name = $this->getFQN($node);
         if (empty($name)) {
-            
+
             return false;
         }
-        
+
         $exists = isset($this->vars[$name]);
-               
+
         if (!$exists || $this->isProperty($name)) {
             $rawName = $this->getLocalName($node);
             $exists = isset($this->vars[$rawName]);
         }
-        
+
         return $exists;
     }
-    
+
     private function getLocalName(\PhpParser\Node $node)
     {
         if ($node instanceof \PhpParser\Node\Stmt\Property) {
             return $node->props[0]->name;
         }
-        
+
         if (isset($node->name) && $node->name == 'this') {
             return '';
         }
-        
-        return is_string($node->name) ? $node->name : (string)$node->name->name;
+
+        return $this->getNodeName($node->name);
     }
-    
-    private function getFQN(\PhpParser\Node $node) : string
+
+    private function getFQN(\PhpParser\Node $node): string
     {
         if ($node instanceof \PhpParser\Node\Stmt\Property) {
             return $node->props[0]->name;
         }
-        
+
         $name = $this->getLocalName($node);
         if (isset($node->var)) {
-            $name = $node->var->name . '.' . $name;
+            $name = $this->getNodeName($node->var->name) . '.' . $name;
         }
-        
+
         return $name;
+    }
+
+    private function getNodeName($node)
+    {
+        if (is_string($node))
+            return $node;
+        
+        if ($node instanceof \PhpParser\Node\Expr\Variable)
+            return $node->name;
+        if ($node instanceof \PhpParser\Node\Scalar\String_)
+            return $node->value;
     }
 
     private function isProperty(string $name)
